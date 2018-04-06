c6679156f38c9527cc0381d7a49e1954b13149a5
Phil Brandvold <phillip.brandvold@gmail.com>
2018-04-06 15:59:12 -0500

    Grandma ignores until three BYE's in a row

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,11 +1,21 @@
 speak_up = "HUH?! SPEAK UP, SONNY!"
 year = rand(1930..1950).to_s;
 response = "NO, NOT SINCE #{year}!"
+bye_counter = 0
 
-input = gets.chomp
-while input.upcase != true
-  puts speak_up
+while bye_counter < 2
   input = gets.chomp
+  if input == 'BYE'
+    bye_counter += 1
+    input = input.upcase
+  else
+    if input != input.upcase
+      bye_counter = 0
+      puts speak_up
+      input = gets.chomp
+    else
+      puts response
+    end
+  end
 end
-
-puts response
+puts 'BYE NOW'
